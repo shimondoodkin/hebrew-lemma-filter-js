6857ffb0f73d03667f083b502c6e22654c2b978d
Shimon Doodkin <helpmepro1@gmail.com>
2013-10-29 14:38:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function unverb(word_ambis)
 {
  for(var i=0;i<word_ambis.length;i++)
  {
-  var word=word_ambis;
+  var word=word_ambis[i];
   if(word[0]=='ל')word_ambis.push(word.substring(1,word.length)+'ה');
   if(word.substring(0,2)=='הת')word_ambis.push(word.substring(1,word.length)+'ה');
   if(word.substring(0,2)=='מת')word_ambis.push(word.substring(1,word.length)+'ה');
@@ -42,7 +42,7 @@ function unplural(word_ambis)
 {
  for(var i=0;i<word_ambis.length;i++)
  {
-  var word=word_ambis;
+  var word=word_ambis[i];
   if(word.substring(word.length-2,word.length)=='ים')word_ambis.push(word.substring(0,word.length-2)+'י');
   if(word.substring(word.length-2,word.length)=='יות')word_ambis.push(word.substring(0,word.length-3)+'ת');
  }
@@ -52,7 +52,7 @@ function ungender(word_ambis)
 {
  for(var i=0;i<word_ambis.length;i++)
  {
-  var word=word_ambis;
+  var word=word_ambis[i];
   if(word.substring(word.length-2,word.length)=='ם')word_ambis.push(word.substring(0,word.length-1));
   if(word.substring(word.length-2,word.length)=='ות')word_ambis.push(word.substring(0,word.length-2));
  }
